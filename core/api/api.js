39a2442210c5777b7fb609825be1053660d85c03
Reza Omidian <romidian70@gmail.com>
2020-03-25 17:04:32 +0430

    user interface support on core

--- core/api/api.js
+++ core/api/api.js
@@ -3,8 +3,8 @@ const
     AutoLoad = require('fastify-autoload'),
     path = require('path'),
     multer = require('fastify-multer'),
-    io = require('socket.io')(fastify.server);
-
+    io = require('socket.io')(fastify.server),
+    fs = require('fs');
 
 fastify.decorate(`bodyParser`, (entity, selects) => {
     let res = {};
@@ -20,6 +20,18 @@ fastify.decorate(`socket`, io);
 fastify.register(multer.contentParser);
 fastify.register(require('fastify-formbody'));
 fastify.register(require('fastify-cors'), {origin: `*`});
+
+//create /public/ static file router
+fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, '../../ui/public'),
+    prefix: '/public/', // optional: default '/'
+});
+
+//create / route streamer for isp.html file
+fastify.get('/', function (req, reply) {
+    const streamer = fs.createReadStream(path.join(__dirname, `../../ui/isp.html`));
+    reply.type('text/html').send(streamer) // streaming  directly
+});
 
 fastify.register(AutoLoad, {
     dir: path.join(__dirname, './routes'),
